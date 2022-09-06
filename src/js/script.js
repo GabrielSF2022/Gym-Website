282209a073b8495169dd96519579a0f167a5f4da
GABRIEL DE SOUZA FURTADO <97768716+GabrielSF2022@users.noreply.github.com>
2022-09-06 14:42:27 -0300

    Update script.js

--- src/js/script.js
+++ src/js/script.js
@@ -162,10 +162,15 @@ contactForm.addEventListener('submit', sendEmail)
 
 
 // SCROLL REVEAL
-function scrollReveal(){
-    ScrollReveal({reset:true});
+const sr = ScrollReveal({
+    origin:'top',
+    distance: '60px',
+    duration: 2500,
+    delay: 400,
+})
 
-    ScrollReveal().reveal('.home__data', { delay:400});
-}
-
-ScrollReveal()
+sr.reveal(`.home__data, .footer__container, .footer__group`)
+sr.reveal(`.home__img`, {delay:700, origin: 'bottom'})
+sr.reveal(`.logos__img, .program__card, .pricing__card`, {interval: 100})
+sr.reveal(`.choose__img, .calculate__content`, {origin: 'left'})
+sr.reveal(`.choose__content, .calculate__img`, {origin: 'right'})
